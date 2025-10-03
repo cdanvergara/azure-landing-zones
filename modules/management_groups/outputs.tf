80a2285c88f8f26f8ec0dc51c1025bac3d42ef21
Cristian Vergara <cristian.vergara@microsoft.com>
2025-10-03 13:26:22 -0500

    fixed

--- modules/management_groups/outputs.tf
+++ modules/management_groups/outputs.tf
@@ -1,7 +1,6 @@
 output "management_group_ids" {
   description = "Map of management group names to their IDs"
   value = {
-    root           = azurerm_management_group.root.id
     ti             = azurerm_management_group.ti.id
     infra          = azurerm_management_group.infra.id
     management     = azurerm_management_group.management.id
@@ -12,8 +11,8 @@ output "management_group_ids" {
 }
 
 output "root_management_group_id" {
-  description = "ID of the root management group"
-  value       = azurerm_management_group.root.id
+  description = "ID of the TI management group (top-level custom management group)"
+  value       = azurerm_management_group.ti.id
 }
 
 output "ti_management_group_id" {
